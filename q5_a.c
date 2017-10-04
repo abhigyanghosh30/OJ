15910af51a07c6a386554a9819aea844fe5d42ad
Abhigyan Ghosh <30973042+abhigyanghosh30@users.noreply.github.com>
2017-10-04 17:45:42 +0000

    Add files via upload

---
+++ q5_a.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	for(;t>0;t--)
+	{
+		int n,k;
+		scanf("%d %d",&n,&k);
+		int a[n],b[1000000];
+		int i;
+		for(i=0;i<1000000;i++)
+			b[i]=0;
+		int max=0;
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&a[i]);
+			b[a[i]]++;
+			max=a[i]>max?a[i]:max;
+		}
+		long int sum=0;
+		int c=0;
+		for(i=max;i>=0;i--)
+		{
+			if(b[i]>0)
+			{
+				int j;
+				for(j=1;j<=b[i];j++)
+				{
+					c++;
+					sum+=i;
+					if(sum>=k)
+						break;
+				}
+				if(sum>=k)
+					break;
+			}
+		}
+		if(c==0)
+			printf("-1\n");
+		else 
+			printf("%d\n",c);
+	}
+	return 0;
+}
